0e775c1c0634bc1120ca75a09955837819a5ff5f
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-06-03 10:29:45 +0200

    bank object

--- main.dart
+++ main.dart
@@ -1,8 +1,15 @@
+import 'encapsulation/piggy_bank.dart';
 import 'polymorphism/super/food.dart';
 import 'polymorphism/sub/pasta.dart';
 import 'polymorphism/sub/pizza.dart';
 
 void main() {
+  //Encapulation
+  PiggyBank bank = PiggyBank(0);
+  bank.initialAmount = 0;
+  bank.deposit(1);
+  print("Current amount in the piggy bank: ${bank.currentAmount}");
+
   //polymorphism
   //Many Forms
   Food pasta = Pasta("Bolognese", 14.50, "Bolognese");
